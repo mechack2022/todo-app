a16b44ac56af4e344c3c14d44a41ffb5adc394d2
mechack200 <taiwogboyegun@gmmail.com>
2022-03-27 23:32:51 +0100

    item component implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
-
   filter: "all" | "active" | "done" = "all";
 
   allItems =[ {description: "eat", done :true}, 
--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Item } from './../item';
+import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent {
+   editable = false;
+   @Input() item!: Item;
+   @Input() newItem!: string;
+   @Output() remove = new  EventEmitter<Item>()
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+   saveItem(description:any){
+     if(!description)return;
+     this.editable = false;
+     this.item.description = description
+   }
 
 }
