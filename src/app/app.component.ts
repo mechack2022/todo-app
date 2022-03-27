e6d79778ac92410e272125ec1141da8e7e3fd154
mechack200 <taiwogboyegun@gmmail.com>
2022-03-27 22:50:56 +0100

    item component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello';
+  title = 'todo';
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems =[ {description: "eat", done :true}, 
+  {description:"sleep", done: false}, 
+  {description:"play", done :true}, 
+  {description:"laugh", done:true}]
+
+  // get items 
+  getItems(){
+    if(this.filter === "all"){
+      return this.allItems
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done :!item.done);
+  }
+  // add item to the list 
+  addItem(todo:string){
+   this.allItems.unshift({
+     description:todo, done :false
+   })
+  }
+  //remove item from list
+  removeItem(item :any){
+ this.allItems.splice(this.allItems.indexOf(item), 1)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ItemComponent,
    
   ],
   imports: [
